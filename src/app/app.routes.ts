90b74a7b79af38cf88a9555e642d8363db121dde
Darek Sańczyk <tantyk@gmail.com>
2024-05-20 22:56:17 +0200

    Auto-generated commit

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { PeopleComponent } from './components/people/people.component';
-import { AboutComponent } from './components/about/about.component';
-
-export const routes: Routes = [
-  { path: 'people', title: 'People', component: PeopleComponent },
-  { path: 'about', title: 'About', component: AboutComponent },
-  { path: '**', redirectTo: '/people' },
-];
