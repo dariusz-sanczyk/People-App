53a3c6eb69567e0c289c13071627cfe70e20b8a5
Darek Sańczyk <tantyk@gmail.com>
2024-05-21 15:57:21 +0200

    Auto-generated commit

---
+++ chunk-3XW6XFVG.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-3MEW5H7R.js";import"./chunk-4OQ54XUN.js";export{a as AboutComponent};
---
+++ chunk-LM7MXAYX.js
@@ -0,0 +1,1 @@
+import{l as a}from"./chunk-433L5AOC.js";import"./chunk-4OQ54XUN.js";export{a as PeopleComponent};
